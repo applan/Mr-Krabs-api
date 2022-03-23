7bb2af477bb633897816e89ec32768be8a1aa859
applan <applan_siepe@naver.com>
2022-03-23 21:45:33 +0900

    2022-03-23 mrkrabs 프로젝트 생성

---
+++ src/main/java/kr/co/mrkrabs/MrKrabsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.mrkrabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrKrabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrKrabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/mrkrabs/MrKrabsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.mrkrabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrKrabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
