c1ea35d13aa661f5f5a73f2401785d28ee356bde
applan <applan_siepe@naver.com>
2022-05-10 01:00:43 +0900

    2022-05-10
    프로젝트에 사용할 DB Name 상수 값 정리한 Class 생성

---
+++ src/main/java/kr/co/mrkrabs/core/global/constant/ProjectDBConstant.java
@@ -0,0 +1,20 @@
+package kr.co.mrkrabs.core.global.constant;
+
+/**
+ * <pre>
+ *
+ * 설명: Project DB Table 명을 저장하는 상수 값 정리
+ *
+ * - 수정 이력 -
+ * --------------------------------------------------------
+ * [ 2022-05-10 applan I ] 최초 작성
+ * --------------------------------------------------------
+ * </pre>
+ * @author applan
+ * @version 1.0
+ */
+public final class ProjectDBConstant {
+
+    public static final String CARD = "card";
+
+}
